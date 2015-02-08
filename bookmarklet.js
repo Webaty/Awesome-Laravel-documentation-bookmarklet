69665efff0fc66a78a08d1f610ff76a3aeb250af
Ayman Gado <aymangado@gmail.com>
2015-02-08 03:24:45 +0200

    Fix links hash

--- bookmarklet.js
+++ bookmarklet.js
@@ -100,11 +100,14 @@ var WebatyLaravelDocumentation = {
 		$('.docs article a[href*=#]:not([href=#])').click(function() {
 			if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
 				var target = $(this.hash);
+				var hash = this.hash.slice(1);
 				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 				if (target.length) {
 					$('html,body').animate({
 						scrollTop: target.offset().top
-					}, 1000);
+					}, 1000, function(){
+						window.location.hash = hash;
+					});
 					return false;
 				}
 			}
